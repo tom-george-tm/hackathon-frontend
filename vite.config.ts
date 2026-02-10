a80e00bbf2a6e3af228dc57a6bbbe41fd653d78e
tom-george-tm <tom.george@thoughtminds.io>
2026-02-10 06:28:54 +0000

    feat: update VITE_BASE_URL, add VITE_APP_NAME and NODE_ENV, and configure Vite preview port.

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,29 @@
-import path from "path"
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, __dirname, '');
-  
-  const target = env.VITE_BASE_URL || 'http://localhost:3000';
-  console.log('🚀 Proxy target:', target);
+  const env = loadEnv(mode, __dirname, "");
+
+  const target = env.VITE_BASE_URL || "http://localhost:3000";
+  console.log("🚀 Proxy target:", target);
 
   return {
     plugins: [react()],
+
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
       },
     },
+
     server: {
       host: true,
-      port: 5173,
-    }
-  }
-})
+    },
 
+    preview: {
+      port: 5173,
+    },
+  };
+});
